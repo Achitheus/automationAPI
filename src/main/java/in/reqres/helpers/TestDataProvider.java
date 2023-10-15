ef42eca9a97cd65ea9fd08a8c33c4fa890d3a7eb
Yuri <yur_yurch@mail.ru>
2023-10-15 22:31:07 +0300

    parametrizing; fixes; tests javadoc

---
+++ src/main/java/in/reqres/helpers/TestDataProvider.java
@@ -0,0 +1,21 @@
+package in.reqres.helpers;
+
+import in.reqres.dto.LogIn;
+import org.testng.annotations.DataProvider;
+
+public class TestDataProvider {
+    @DataProvider(name = "page_numbers")
+    public Object[] pageNumbers() {
+        return new Object[]{1, 2};
+    }
+
+    @DataProvider(name = "xml_tag_count_checker")
+    public Object[][] xmlAndTagCountSets() {
+        return new Object[][]{{"", 14}};
+    }
+
+    @DataProvider(name = "correct_log_ins")
+    public Object[] correctLogIns(){
+        return new Object[]{new LogIn("eve.holt@reqres.in", "cityslicka")};
+    }
+}
--- src/test/java/in/reqres/ApiTests.java
+++ src/test/java/in/reqres/ApiTests.java
@@ -1,13 +1,12 @@
 package in.reqres;
 
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.google.common.collect.Comparators;
 import in.reqres.dto.ColorStyle;
 import in.reqres.dto.LogIn;
 import in.reqres.dto.Resource;
 import in.reqres.dto.UserData;
-import io.qameta.allure.Step;
+import in.reqres.helpers.TestDataProvider;
+import io.restassured.common.mapper.TypeRef;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -30,39 +29,47 @@ public class ApiTests {
         deleteSpec();
     }
 
-    @Test
-    public void checkUniqueAvatarFileNames() {
-        JavaType type = TypeFactory.defaultInstance()
-                .constructParametricType(Resource.class, UserData.class);
+    /**
+     * Используя сервис <a href="https://reqres.in/">reqres.in</a> получает список пользователей
+     * со страницы {@code pageNumber} и
+     * убеждается, что имена файлов аватаров пользователей уникальны.
+     */
+    @Test(dataProvider = "page_numbers", dataProviderClass = TestDataProvider.class)
+    public void checkUniqueAvatarFileNames(Integer pageNumber) {
         Resource<UserData> resource =
                 given()
                         .spec(requestSpec())
                         .when()
-                        .get("api/users?page=2")
+                        .get("api/users?page=" + pageNumber)
                         .then()
                         .spec(responseSpec())
-                        .extract().body().as(type);
+                        .extract().body().as(new TypeRef<>() {
+                        });
 
         Set<String> uniqueAvatarFileNames = resource.getData().stream()
                 .map(UserData::getAvatar)
                 .map(ava -> ava.replaceAll("^.+/|\\..+", ""))
                 .collect(Collectors.toSet());
         Assert.assertEquals(resource.getData().size(), uniqueAvatarFileNames.size(),
-                "Количество объектов списка должно совпадать с количеством уникальных объектов этого же списка");
+                "(стр. №" + pageNumber + ") Количество объектов списка должно совпадать" +
+                        " с количеством уникальных объектов этого же списка");
     }
 
+    /**
+     * Убеждается в том, что тест {@link #checkUniqueAvatarFileNames(Integer)} работает корректно
+     * и падает, если присутствует хотя бы один повтор какого-либо имени файла.
+     */
     @Test
-    public void avatarFileNamesNotUnique() {
+    public void notUniqueAvatarFileNamesDetecting() {
         installSpec(requestSpec(), responseSpec());
-        JavaType type = TypeFactory.defaultInstance()
-                .constructParametricType(Resource.class, UserData.class);
 
         Resource<UserData> resource =
                 given()
                         .when()
-                        .get("api/users?page=2")
+                        .get("api/users?page=1")
                         .then()
-                        .extract().body().as(type);
+                        .extract().body().as(new TypeRef<>() {
+                        });
 
         List<UserData> notUniqueList = resource.getData();
         Assert.assertTrue(notUniqueList.size() > 1,
@@ -76,63 +83,78 @@ public class ApiTests {
                 "Уникальных объектов должно быть меньше их общего количества");
     }
 
-    @Test
-    public void successfulAuthorization() {
+    /**
+     * Используя сервис <a href="https://reqres.in/">reqres.in</a> тестирует
+     * авторизацию пользователя в системе на успешный логин данными из
+     * {@code logIn}.
+     */
+    @Test(dataProvider = "correct_log_ins", dataProviderClass = TestDataProvider.class)
+    public void successfulAuthorization(LogIn logIn) {
         installSpec(requestSpec(), responseSpec());
-        LogIn login = new LogIn("eve.holt@reqres.in", "cityslicka");
         given()
-                .body(login)
+                .body(logIn)
                 .when()
                 .post("api/login")
                 .then()
                 .assertThat().body("token", not(emptyOrNullString()));
     }
 
+    /**
+     * Используя сервис <a href="https://reqres.in/">reqres.in</a> тестирует авторизацию пользователя в системе
+     * на логин с ошибкой из-за не введённого пароля.
+     */
     @Test
     public void unsuccessfulAuthorization() {
         installSpec(requestSpec());
-        LogIn login = new LogIn("eve.holt@reqres.in", "");
         given()
-                .body(login)
+                .body(new LogIn("eve.holt@reqres.in", ""))
                 .when()
                 .post("api/login")
                 .then()
-                .assertThat().statusCode(400).and().body("error", not(emptyOrNullString()));
+                .assertThat().statusCode(400)
+                .and().body("error", not(emptyOrNullString()));
     }
-@Step
+
+    /**
+     * Используя сервис <a href="https://reqres.in/">reqres.in</a> убеждается,
+     * что операция LIST<RESOURCE> возвращает данные отсортированные по годам.
+     */
     @Test
     public void resourceSortedByYears() {
         installSpec(requestSpec(), responseSpec());
-        JavaType type = TypeFactory.defaultInstance()
-                .constructParametricType(Resource.class, ColorStyle.class);
+
         Resource<ColorStyle> resource =
                 given()
                         .when()
                         .get("api/unknown")
                         .then()
                         .assertThat().body("data.year", not(hasItem(nullValue())))
-                        .extract().body().as(type);
+                        .extract().body().as(new TypeRef<>() {
+                        });
         boolean isInOrder = Comparators.isInOrder(resource.getData(), Comparator.comparingLong(ColorStyle::getYear));
-        Assert.assertTrue(isInOrder);
+        Assert.assertTrue(isInOrder, "Элементы данных должны быть упорядочены по возрастанию");
     }
 
-    @Test
-    public void checkTagCount() {
-        int expectedTagCount = 14;
+    /**
+     * Используя сервис <a href="https://gateway.autodns.com/">gateway.autodns.com</a> убедиться,
+     * что xml-ресурс {@code resourcePath} содержит количество тегов равное {@code  tagCount}.
+     */
+    @Test(dataProvider = "xml_tag_count_checker", dataProviderClass = TestDataProvider.class)
+    public void checkTagCount(String resourcePath, Integer tagCount) {
         String xml =
                 given()
                         .when()
-                        .get("https://gateway.autodns.com/")
+                        .get("https://gateway.autodns.com/" + resourcePath)
                         .then()
                         .statusCode(200)
                         .extract().body().asString();
-        Pattern tagPattern = Pattern.compile("<\\w+>");
+        Pattern tagPattern = Pattern.compile("<[^!?/][^<]+>");
         Matcher matcher = tagPattern.matcher(xml);
         List<String> tags = matcher.results()
                 .map(MatchResult::group)
                 .collect(Collectors.toList());
-        Assert.assertEquals(tags.size(), expectedTagCount,
-                "Ожидалось " + expectedTagCount + " тегов, а реально " + tags.size());
+        Assert.assertEquals(tags.size(), tagCount,
+                "Ожидалось " + tagCount + " тегов, а реально " + tags.size());
     }
 
 }
